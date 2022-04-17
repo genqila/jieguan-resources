e1f6b881d01e4442f233e7ae9b0bc3360b7930bd
Genqila <102831561+genqila@users.noreply.github.com>
2022-04-18 00:11:20 +0200

    feat(benchmarks): add areas

---
+++ benchmarks/areas.lua
@@ -0,0 +1,94 @@
+--[[ #### Script Selection ####
+Set true to run the script. Set false to not run the script.
+DO NOT CHANGE BOT ORDER - THIS WILL MAKE ERRORS ]]--
+
+--[[ ACT 1 ]]--
+Config.Scripts.Cows.Enabled                   = false
+
+Config.Scripts.Mausoleum.Enabled              = true
+Config.Scripts.Mausoleum.ClearCrypt           = false
+
+Config.Scripts.Countess.Enabled               = true
+
+Config.Scripts.UndergroundPassage.Enabled     = false
+
+Config.Scripts.Pits.Enabled                   = false
+Config.Scripts.Pits.ClearPits1                = false
+
+Config.Scripts.Andariel.Enabled               = true
+
+--[[ ACT 2 ]]--
+Config.Scripts.StonyTombs.Enabled             = true
+Config.Scripts.StonyTombs.ClearStonyTombs2    = true
+
+Config.Scripts.AncientTunnels.Enabled         = true
+
+Config.Scripts.Arcane.Enabled                 = false
+
+Config.Scripts.Summoner.Enabled               = true
+Config.Scripts.Summoner.KillDuriel            = false
+
+Config.Scripts.Tombs.Enabled                  = false
+
+Config.Scripts.Duriel.Enabled                 = false
+
+--[[ ACT 3 ]]--
+Config.Scripts.ArachnidLair.Enabled           = true
+
+Config.Scripts.SwampyPits.Enabled             = false
+Config.Scripts.SwampyPits.ClearSwampyPits2    = false
+Config.Scripts.SwampyPits.ClearSwampyPits3    = false
+
+Config.Scripts.DisusedFane.Enabled            = false
+
+Config.Scripts.RuinedTemple.Enabled           = false
+
+Config.Scripts.ForgottenReliquary.Enabled     = false
+
+Config.Scripts.A3Sewers.Enabled               = false
+
+-- get area id -> https://github.com/kolton/d2bot-with-kolbot/blob/master/d2bs/kolbot/sdk/areas.txt
+Config.Scripts.ChestMania.Enabled             = true
+Config.Scripts.ChestMania.Act1                = { }
+Config.Scripts.ChestMania.Act2                = { }
+Config.Scripts.ChestMania.Act3                = { 79, 80, 81 } -- Lower Kurast, Kurast Bazaar, Upper Kurast
+Config.Scripts.ChestMania.Act4                = { }
+Config.Scripts.ChestMania.Act5                = { }
+
+Config.Scripts.Travincal.Enabled              = true
+
+Config.Scripts.Mephisto.Enabled               = true
+Config.Scripts.Mephisto.KillCouncil           = true
+Config.Scripts.Mephisto.TakeRedPortal         = true
+
+--[[ ACT 4 ]]--
+Config.Scripts.Diablo.Enabled                 = false
+Config.Scripts.Diablo.Entrance                = false
+
+Config.Scripts.FastDiablo.Enabled             = true
+
+--[[ ACT 5 ]]--
+Config.Scripts.Abaddon.Enabled                = false
+
+Config.Scripts.PitOfAcheron.Enabled           = false
+
+Config.Scripts.InfernalPit.Enabled            = false
+
+Config.Scripts.DrifterCavern.Enabled          = false
+
+Config.Scripts.IcyCellar.Enabled              = false
+
+Config.Scripts.Eldritch.Enabled               = false
+Config.Scripts.Eldritch.KillShenk             = false
+
+Config.Scripts.Pindleskin.Enabled             = true
+Config.Scripts.Pindleskin.KillNihlathak       = false
+
+Config.Scripts.Nihlathak.Enabled              = false
+
+Config.Scripts.Worldstone.Enabled             = false
+
+Config.Scripts.Baal.Enabled                   = true
+Config.Scripts.Baal.DollQuit                  = false
+Config.Scripts.Baal.SoulQuit                  = false
+Config.Scripts.Baal.KillBaal                  = true
