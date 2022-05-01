6ecabca07d272f819c06ececb846f1f0521e8c11
Genqila <102831561+genqila@users.noreply.github.com>
2022-05-01 16:09:48 +0200

    feat(configs): add areas

---
+++ configs/areas/01-nightmare.lua
@@ -0,0 +1,101 @@
+--[[ #### Script Selection ####
+Set true to run the script. Set false to not run the script.
+DO NOT CHANGE BOT ORDER - THIS WILL MAKE ERRORS ]]--
+
+--[[ ACT 1 ]]--
+Config.Scripts.Cows.Enabled                   = true
+
+Config.Scripts.Mausoleum.Enabled              = false
+Config.Scripts.Mausoleum.ClearCrypt           = false
+
+Config.Scripts.Countess.Enabled               = false
+
+Config.Scripts.UndergroundPassage.Enabled     = false
+
+Config.Scripts.Pits.Enabled                   = false
+Config.Scripts.Pits.ClearPits1                = false
+
+Config.Scripts.Andariel.Enabled               = true
+
+--[[ ACT 2 ]]--
+Config.Scripts.StonyTombs.Enabled             = false
+Config.Scripts.StonyTombs.ClearStonyTombs2    = false
+
+Config.Scripts.AncientTunnels.Enabled         = false
+
+Config.Scripts.Arcane.Enabled                 = false
+
+Config.Scripts.Summoner.Enabled               = false
+Config.Scripts.Summoner.KillDuriel            = false
+
+Config.Scripts.Tombs.Enabled                  = false
+
+Config.Scripts.Duriel.Enabled                 = false
+
+--[[ ACT 3 ]]--
+Config.Scripts.ArachnidLair.Enabled           = false
+
+Config.Scripts.SwampyPits.Enabled             = false
+Config.Scripts.SwampyPits.ClearSwampyPits2    = false
+Config.Scripts.SwampyPits.ClearSwampyPits3    = false
+
+Config.Scripts.DisusedFane.Enabled            = false
+
+Config.Scripts.RuinedTemple.Enabled           = false
+
+Config.Scripts.ForgottenReliquary.Enabled     = false
+
+Config.Scripts.A3Sewers.Enabled               = false
+
+-- get area id -> https://github.com/kolton/d2bot-with-kolbot/blob/master/d2bs/kolbot/sdk/areas.txt
+Config.Scripts.ChestMania.Enabled             = false
+Config.Scripts.ChestMania.Act1                = { }
+Config.Scripts.ChestMania.Act2                = { }
+Config.Scripts.ChestMania.Act3                = { }
+Config.Scripts.ChestMania.Act4                = { }
+Config.Scripts.ChestMania.Act5                = { }
+
+Config.Scripts.Travincal.Enabled              = false
+
+Config.Scripts.Mephisto.Enabled               = true
+Config.Scripts.Mephisto.MoatTrick             = false
+Config.Scripts.Mephisto.KillCouncil           = false
+Config.Scripts.Mephisto.TakeRedPortal         = true
+
+--[[ ACT 4 ]]--
+Config.Scripts.Diablo.Enabled                 = true
+Config.Scripts.Diablo.Entrance                = false
+
+Config.Scripts.FastDiablo.Enabled             = false
+
+--[[ ACT 5 ]]--
+Config.Scripts.Abaddon.Enabled                = false
+
+Config.Scripts.PitOfAcheron.Enabled           = false
+
+Config.Scripts.InfernalPit.Enabled            = false
+
+Config.Scripts.DrifterCavern.Enabled          = false
+
+Config.Scripts.IcyCellar.Enabled              = false
+
+Config.Scripts.Eldritch.Enabled               = true
+Config.Scripts.Eldritch.KillShenk             = true
+
+Config.Scripts.Pindleskin.Enabled             = true
+Config.Scripts.Pindleskin.KillNihlathak       = false
+
+Config.Scripts.Nihlathak.Enabled              = false
+
+Config.Scripts.Worldstone.Enabled             = false
+
+Config.Scripts.Baal.Enabled                   = true
+Config.Scripts.Baal.DollQuit                  = false
+Config.Scripts.Baal.SoulQuit                  = false
+Config.Scripts.Baal.KillBaal                  = true
+
+
+Config.BossPriority       = false
+Config.ClearPath.Enabled  = false
+Config.ClearPath.Areas    = {
+}
---
+++ configs/areas/04-hell-endgame.lua
@@ -0,0 +1,105 @@
+--[[ #### Script Selection ####
+Set true to run the script. Set false to not run the script.
+DO NOT CHANGE BOT ORDER - THIS WILL MAKE ERRORS ]]--
+
+--[[ ACT 1 ]]--
+Config.Scripts.Cows.Enabled                   = false
+
+Config.Scripts.Mausoleum.Enabled              = false
+Config.Scripts.Mausoleum.ClearCrypt           = false
+
+Config.Scripts.Countess.Enabled               = true
+
+Config.Scripts.UndergroundPassage.Enabled     = false
+
+Config.Scripts.Pits.Enabled                   = false
+Config.Scripts.Pits.ClearPits1                = false
+
+Config.Scripts.Andariel.Enabled               = false
+
+--[[ ACT 2 ]]--
+Config.Scripts.StonyTombs.Enabled             = false
+Config.Scripts.StonyTombs.ClearStonyTombs2    = false
+
+Config.Scripts.AncientTunnels.Enabled         = false
+
+Config.Scripts.Arcane.Enabled                 = false
+
+Config.Scripts.Summoner.Enabled               = true
+Config.Scripts.Summoner.KillDuriel            = true
+
+Config.Scripts.Tombs.Enabled                  = false
+
+Config.Scripts.Duriel.Enabled                 = false
+
+--[[ ACT 3 ]]--
+Config.Scripts.ArachnidLair.Enabled           = false
+
+Config.Scripts.SwampyPits.Enabled             = false
+Config.Scripts.SwampyPits.ClearSwampyPits2    = false
+Config.Scripts.SwampyPits.ClearSwampyPits3    = false
+
+Config.Scripts.DisusedFane.Enabled            = false
+
+Config.Scripts.RuinedTemple.Enabled           = false
+
+Config.Scripts.ForgottenReliquary.Enabled     = false
+
+Config.Scripts.A3Sewers.Enabled               = false
+
+-- get area id -> https://github.com/kolton/d2bot-with-kolbot/blob/master/d2bs/kolbot/sdk/areas.txt
+Config.Scripts.ChestMania.Enabled             = false
+Config.Scripts.ChestMania.Act1                = { }
+Config.Scripts.ChestMania.Act2                = { }
+Config.Scripts.ChestMania.Act3                = { }
+Config.Scripts.ChestMania.Act4                = { }
+Config.Scripts.ChestMania.Act5                = { }
+
+Config.Scripts.Travincal.Enabled              = false
+
+Config.Scripts.Mephisto.Enabled               = true
+Config.Scripts.Mephisto.MoatTrick             = false
+Config.Scripts.Mephisto.KillCouncil           = true
+Config.Scripts.Mephisto.TakeRedPortal         = true
+
+--[[ ACT 4 ]]--
+Config.Scripts.Diablo.Enabled                 = false
+Config.Scripts.Diablo.Entrance                = false
+
+Config.Scripts.FastDiablo.Enabled             = true
+
+--[[ ACT 5 ]]--
+Config.Scripts.Abaddon.Enabled                = false
+
+Config.Scripts.PitOfAcheron.Enabled           = false
+
+Config.Scripts.InfernalPit.Enabled            = false
+
+Config.Scripts.DrifterCavern.Enabled          = false
+
+Config.Scripts.IcyCellar.Enabled              = false
+
+Config.Scripts.Eldritch.Enabled               = false
+Config.Scripts.Eldritch.KillShenk             = false
+
+Config.Scripts.Pindleskin.Enabled             = true
+Config.Scripts.Pindleskin.KillNihlathak       = false
+
+Config.Scripts.Nihlathak.Enabled              = true
+
+Config.Scripts.Worldstone.Enabled             = false
+
+Config.Scripts.Baal.Enabled                   = true
+Config.Scripts.Baal.DollQuit                  = false
+Config.Scripts.Baal.SoulQuit                  = false
+Config.Scripts.Baal.KillBaal                  = true
+
+Config.BossPriority       = true
+Config.ClearPath.Enabled  = true
+Config.ClearPath.Areas    = {
+    108, -- Chaos Sanctuary
+
+    129, -- Worldstone Keep Level 2
+    130, -- Worldstone Keep Level 3
+    131, -- Throne of Destruction
+}
